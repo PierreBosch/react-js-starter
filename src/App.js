356237f9b02a08bab526e66cf57331b18fd7b8ed
Luis Felipe Mattos de Brito Souza <luis.fmbs@outlook.com>
2018-11-27 11:30:47 -0200

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
+import React from 'react';
+import Header from "./components/Header/Header";
+import "./styles.css";
+import Routes from "./routes";
+const App = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+            <Header/>
+            <Routes />
+        </div>
     );
-  }
-}
+};
 
 export default App;
